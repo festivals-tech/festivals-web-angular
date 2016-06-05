1e14d117195851e67856c0f20c9a71ccea0befb1
Zenedith <github@zenedith.mailgun.org>
2016-06-05 10:48:13 +0200

    Updated dependencies

--- server.js
+++ server.js
@@ -2,5 +2,10 @@ var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
 
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+var options = {
+  maxAge : 86400000,
+  clientMaxAge : 604800000
+};
+
+app.use(gzippo.staticGzip("" + __dirname + "/dist", options));
 app.listen(process.env.PORT || 5000);
